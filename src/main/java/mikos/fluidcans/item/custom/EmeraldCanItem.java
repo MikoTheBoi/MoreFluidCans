139b56b02817bcbc1ef02369e94f792ad5cff824
MikoTheBoi <mikociec@gmail.com>
2026-01-05 14:24:41 +0100

    *Probably* fixed can amounts?

--- src/main/java/mikos/fluidcans/item/custom/EmeraldCanItem.java
+++ src/main/java/mikos/fluidcans/item/custom/EmeraldCanItem.java
@@ -1,8 +1,24 @@
 package mikos.fluidcans.item.custom;
 
+import io.github.fabricators_of_create.porting_lib.util.FluidStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
 /**
  * Fluid container holding a gem of fluid
  */
+@SuppressWarnings("removal")
 public class EmeraldCanItem extends CopperCanItem {
 
   public static long CAPACITY = 8100L;
@@ -10,4 +26,23 @@ public class EmeraldCanItem extends CopperCanItem {
   public EmeraldCanItem(Properties properties) {
     super(properties);
   }
+
+  @Override
+  public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+    Fluid fluid = getFluid(stack.getTag());
+    if (fluid != Fluids.EMPTY) {
+      CompoundTag fluidTag = getFluidTag(stack.getTag());
+      MutableComponent text;
+      if (fluidTag != null) {
+        FluidStack displayFluid = new FluidStack(fluid, CAPACITY, fluidTag);
+        text = displayFluid.getDisplayName().plainCopy();
+      } else {
+        text = new TranslatableComponent(fluid.getAttributes().getTranslationKey());
+      }
+      tooltip.add(new TranslatableComponent(this.getDescriptionId() + ".contents", text).withStyle(ChatFormatting.GRAY));
+    } else {
+      tooltip.add(new TranslatableComponent(this.getDescriptionId() + ".tooltip").withStyle(ChatFormatting.GRAY));
+    }
+  }
+
 }
